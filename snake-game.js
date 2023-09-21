8a9904f79d63c72e525fb07e1b326de5f1d5fb15
trentm2202 <129075367+trentm2202@users.noreply.github.com>
2023-09-21 16:52:25 -0500

    Create snake-game.js

---
+++ snake-game.js
@@ -0,0 +1,6 @@
+<!-- Placeholder for the Snake game -->
+<div id="snake-game-container">
+    <canvas id="snake-canvas" width="400" height="400"></canvas>
+</div>
+
+<script src="snake-game.js"></script>
